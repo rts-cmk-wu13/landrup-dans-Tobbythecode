bfccd025609ac1a1f2df257b401807db5d7b1147
Tobias <tobimation23@gmail.com>
2026-02-18 14:24:54 +0100

    main page for login

---
+++ src/app/login/page.jsx
@@ -0,0 +1,13 @@
+import LoginForm from "@/components/LoginForm/LoginForm"
+
+export default function Login() {
+
+    return (
+        <>
+            <h1>Log ind</h1>
+            <LoginForm />
+        </>
+    )
+
+
+}
