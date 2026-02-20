1fd746a22e871a5e0d6861ca1701aad1d67ecd44
Tobias <tobimation23@gmail.com>
2026-02-20 13:51:46 +0100

    lavede ændringer på hold pagen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 
 import Hero from "@/components/hero/hero"
-import Link from 'next/link'
 
+import Hold from "@/components/hold/hold"
+import Image from "next/image"
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center  font-sans ">
-      <main className="flex min-h-screen max-w-3xl flex-col items-center justify-evenly py-32 px-16">
-          <Hero />
-    <Link href="/login" className="mt-10 rounded bg-white px-4 py-2 text-black hover:bg-blue-600">
-      Login
-    </Link>
+    <div className="flex min-h-screen justify-center  font-sans  ">
+      <main className="flex min-h-screen max-w-3xl flex-col items-center justify-evenly ">  
+
+<Hero/>
+ 
+  
+    <Hold/>
       </main>
     </div>
   );
