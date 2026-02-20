509d96a7b09a3e0e7ad9ceda031438fb4058b9e6
Tobias <tobimation23@gmail.com>
2026-02-20 13:54:33 +0100

    kopiret nogle ting fra den gamle opgave

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,39 +1,45 @@
+import { cookies } from "next/headers"
 import Image from "next/image"
 
-export default async function ProfilePage({ user }) {
+export default async function ProfilePage({ params }) {
+  const { id } = params
 
- const response = await fetch(`http://localhost:4000/api/v1/users/${user}`, {
-  "method": "GET",
-  "headers": {
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjo1LCJ1c2VybmFtZSI6InVzZXIxIiwicGFzc3dvcmQiOiIkMmEkMTUkay5nVzZqT005ZXlWSU1kTEMwRy9MLjdQT0J1bGVWNTYzNWNhejdXaDc5VFRkSTBnVWsuT2UiLCJmaXJzdG5hbWUiOiJNYXRoaWxkZSIsImxhc3RuYW1lIjoiUGV0ZXJzZW4iLCJhZ2UiOjE0LCJyb2xlIjoiZGVmYXVsdCIsImNyZWF0ZWRBdCI6IjIwMjEtMDktMjNUMDg6MTI6MDguMDgyWiIsInVwZGF0ZWRBdCI6IjIwMjEtMDktMjNUMDg6MTI6MDguMDgyWiJ9LCJpYXQiOjE3NzEyNjY0NTAsImV4cCI6MTc3MTI3MDA1MH0.79JOC41U3WybwToh2Wm9iSUdYMGrkKb5q6nh0CBpMxs"
+  const cookieStore = await cookies() 
+  const authToken = cookieStore.get("authToken")?.value
+
+  if (!authToken) {
+    return <h1 className="text-center">Not logged in</h1>
   }
-})
-  if (!response.ok) {
+
+  const response = await fetch(
+    `http://localhost:4000/api/v1/users/${id}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${authToken}`
+      },
+      cache: "no-store"
+    }
+  )
+
+if (response.ok) {
+  window.location.href = "/profile" // manually redirect after success
+} else {
+  const data = await response.json()
+  setErrors({ form: data.error })
+}
+  const user = await response.json()
 
   return (
     <>
       <h1 className="text-center">Min profil</h1>
-  
+
       <section className="flex flex-col items-center bg-white">
-       
-<Image
-     
-         
-          src="/user.svg"
-          alt="User profile"
-          width={100}
-          height={100}
-          priority
-        />
-   
-    
- 
-  
-      
+        <Image src="/user.svg" alt="User profile" width={100} height={100} priority />
+
         <p>Name: {user.firstname} {user.lastname}</p>
         <p>Age: {user.age}</p>
       </section>
     </>
-  );
-}
+  )
 }
