e0466e7d9b4d422d2e80abb7a547c9e056b71504
Tobias <tobimation23@gmail.com>
2026-02-20 13:54:59 +0100

    bruger denne her route bare som af teste på for nu af (skal nok brugen den dw)

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,36 +1,38 @@
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = process.env.REDIRECT_URI;
-
 export async function GET(request) {
-    const url = new URL(request.nextUrl);
-    const code = url.searchParams.get("code")
-    //console.log("code", code);
+  const url = request.nextUrl;
+  const code = url.searchParams.get("code");
 
-    const response = await fetch("http://localhost:4000/auth/token", {
-   "method": "POST",
-  "headers": {
-    "Content-Type": "application/json"
-  },
-  "body": {
-    "username": "instructor1",
-    "password": "1234"
+  const response = await fetch("http://localhost:4000/auth/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      username: "instructor1",
+      password: "1234"
+    })
+  });
+
+  if (!response.ok) {
+    return NextResponse.json({ error: "Token request failed" }, { status: 500 });
   }
-})
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
 
-    const data = await response.json();
+  const data = await response.json();
 
-    const cookieStore = await cookies();
+  const cookieStore = cookies();
 
-    
-    cookieStore.set("IPM_AT", data.access_token, { maxAge: data.expires_in });
-    cookieStore.set("IPM_RT", data.refresh_token, { maxAge: data.expires_in * 5 });
+  cookieStore.set("IPM_AT", data.access_token, {
+    maxAge: data.expires_in,
+    httpOnly: true
+  });
 
+  cookieStore.set("IPM_RT", data.refresh_token, {
+    maxAge: data.expires_in * 5,
+    httpOnly: true
+  });
 
-    return NextResponse.redirect(new URL("http://127.0.0.1:3000/"));
+  return NextResponse.redirect(new URL("/", request.url));
 }
