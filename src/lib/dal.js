cb614b59bae64091b0ece26ddfe288a845e37876
Tobias <tobimation23@gmail.com>
2026-02-20 15:54:10 +0100

    fixed my dal to work on the activity pages

--- src/lib/dal.js
+++ src/lib/dal.js
@@ -3,57 +3,23 @@
 
 
 
-export async function getAllBlogPosts() {
-    try {
-        const response = await fetch("http://localhost:4000/posts")
-        if (!response.ok) {
-            throw new Error("Something went wrong");
-        }
-        if (response.status !== 200) {
-            throw new Error(response.statusText);
-        }
-        if (response.headers.get("content-type") === "application/json") {
-            return await response.json();
-        }
-        throw new Error("Unexpected response format");
-    } catch (error) {
-        console.log("getAllBlogPosts", error)
-        return {
-            success: false,
-            message: "Something went wrong on the server try again later"
-        }
+export async function getAllActivities(){
+    const response = await fetch(`http://localhost:4000/api/v1/activities/`)
+
+    if (!response.ok) {
+        throw new Error({ message: "something went wrong" })
     }
+    return await response.json()
+    
+
+}
+export async function getActivityById(id) {
+    const response = await fetch(`http://localhost:4000/api/v1/activities/${id}`)
+
+    if (!response.ok) {
+        throw new Error({ message: "something went wrong" })
+    }
+    return await response.json()
+
 }
 
-export async function  getBlogPostById (id) { 
-  try {
-  if (!id) {  
-    throw new Error({message: "ID is required"});
-  }
-if (!/^\d+$/.test(id)) {
-  throw new Error({message: "incorrect ID format"});
-}
-
-
-  const response = await fetch(`http://localhost:4000/posts/${id}`)
-  if (!response.ok) {
-    throw new Error({message: "Fejl ved hentning af data"});
-  }
-     if (response.status !== 200) {
-      throw new Error({message: response.statusText});
-  }
-
-if (response.headers.get("content-type ") === "application/json") {
-return await response.json();
-     }
-
-
-throw new Error ({message: "NOT JSON"})
-} catch (error) {
-  console.log("getBlogPostById Error", error);
-  return {
-    success: false,
-    message: "something went wrong idk lmao  "
-  }
-}
-}
