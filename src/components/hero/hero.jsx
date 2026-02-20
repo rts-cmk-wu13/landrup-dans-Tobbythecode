946caf394d22d18843d1e521a5b33e053297bcbf
Tobias <tobimation23@gmail.com>
2026-02-20 13:54:08 +0100

    lavede lidt styling på denne her så den er mere fin

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,40 +1,31 @@
 import Image from "next/image";
-
+import Link from "next/link";   
 export default function Hero() {
-return (
-
-
-<>
-
-
-    <figure className="flex content-center">
-<Image 
-        
+  return (
+    <section className="h-screen w-full bg-[url('/heroimg.jpg')] bg-cover bg-no-repeat bg-center flex flex-col items-center justify-center text-white">
+      
+      <figure className="flex justify-center items-center">
+        <Image
           src="/logo.svg"
-          alt="Next.js logo"
+          alt="Logo"
           width={200}
           height={50}
           priority
         />
-</figure>
+      </figure>
 
-
-<div>  
-      <figure className="flex content-center">
-<Image
-        
+      <figure className="flex justify-center items-center mt-6">
+        <Image
           src="/title.svg"
-          alt="Next.js logo"
+          alt="Title"
           width={400}
           height={40}
           priority
         />
-</figure>
-</div>
-
-</>
-
-
-)
-
+      </figure>
+   <Link href="/login" className="mt-10 rounded bg-white px-20 py-5 text-black hover:bg-blue-600">
+      Login
+    </Link>
+    </section>
+  );
 }
