5248fc508cb1f3b1939291a4fdcc383b016760f8
Tobias <tobimation23@gmail.com>
2026-02-23 15:57:23 +0100

    stylede på blogcard /aktiviter pagen som jeg er glad for indtil videre

--- src/app/components/BlogCard/Blog.jsx
+++ src/app/components/BlogCard/Blog.jsx
@@ -1,24 +1,28 @@
 import Link from "next/link";
 
-export default function BlogCard({ activity}) {
-
-    return (
-        <>
-          <div className=" justify-center    font-sans  ">
-            <main className=" max-w-3xl   items-center justify-evenly">
+export default function BlogCard({ activity }) {
+  return (
+    <div className="flex justify-center font-sans my-4">
+      <main className="max-w-3xl flex flex-col items-center gap-4">
         <Link href={`/activities/${activity.id}`}>
-        <article>
-<figure className="flex  justify-center items-center">
-<img className="rounded-lg w-80 h-full" src={activity.asset.url} alt="" />
-</figure>
-<div>
-<h2 id={"activities" + activity.id}>{activity.name} </h2>
-<p>{activity.minAge} + år</p>
+          <article className="flex flex-col items-center gap-3">
+   <div className="relative w-full sm:w-[500px] h-[500px] rounded-lg overflow-hidden">
+              <img
+                className="w-full h-full object-cover"
+                src={activity.asset.url}
+                alt={activity.name}
+              />
+
+           
+<div className="absolute bottom-0 left-0 w-full bg-[rgba(0,49,71,0.75)] text-white p-4 text-left">
+  <h2 className="text-lg font-semibold">{activity.name}</h2>
+  <p className="text-sm">{activity.minAge}+ år</p>
 </div>
-
-
-        </article>
+            </div>
+          
+          </article>
         </Link>
-   </main>    </div> </>
-    )
+      </main>
+    </div>
+  );
 }
