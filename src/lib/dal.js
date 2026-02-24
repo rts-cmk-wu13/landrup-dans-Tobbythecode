fe5940c45cd803ddf6619275bea4c5b423f85191
Tobias <tobimation23@gmail.com>
2026-02-24 15:55:08 +0100

    fik endelig fiksede og satte min exports ind i dal og sat variable inde i selve exporten og ikke udenfor

--- src/lib/dal.js
+++ src/lib/dal.js
@@ -1,4 +1,7 @@
 "use server"
+import { cookies } from "next/headers"
+
+
 
 export async function getAllActivities() {
     try {
@@ -15,15 +18,49 @@ export async function getAllActivities() {
 }
 
 export async function getActivityById(id) {
-    try {
+  
         const response = await fetch(`http://localhost:4000/api/v1/activities/${id}`);
         if (!response.ok) {
             console.error("getActivityById error:", response.status, response.statusText);
             return null;
         }
         return await response.json();
-    } catch (error) {
-        console.error("getActivityById fetch failed:", error);
-        return null;
+    } 
+
+
+export async function getUsersById(userId) 
+{const cookieStore = await cookies()
+const authToken = cookieStore.get("authToken")
+    const response = await fetch(`http://localhost:4000/api/v1/users/${userId}`,
+        {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${authToken.value}`
+            }
+        }
+    )
+    console.log(response);
+
+    if (!response.ok) {
+        throw new Error({ message: "something went wrong" })
     }
+    return await response.json()
 }
+
+// export async function getUserWithActivities(userId, authToken) {
+    
+//     const response = await fetch(`http://localhost:4000/api/v1/users/${userId}`, {
+//         method: "GET",
+//         headers: {
+//             "Content-Type": "application/json",
+//             Authorization: `Bearer ${authToken}`
+//         }
+//     });
+//     if (!response.ok) {
+//     const errorText = await response.text();
+//     console.error("Fetch failed:", response.status, errorText);
+//     throw new Error("Failed to fetch user");
+//     }
+//     return await response.json();
+// }
